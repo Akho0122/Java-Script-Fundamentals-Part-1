b83a8608085741479244aca2a15b627653edb8e0
Akho0122 <mr.akhror.dev@gmail.com>
2022-02-02 22:03:52 +0500

    Conversion and Coercion

--- script.js
+++ script.js
@@ -75,19 +75,39 @@
 // console.log(century);
 
 
-const massMark = 78;
-const heightMark= 1.69;
-const massJohn = 92;
-const heightJohn = 1.95
+// const massMark = 78;
+// const heightMark= 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95
 
-const BMIMark = massMark / heightMark ** 2;
-const BMIJohn = massJohn / (heightJohn * heightJohn);
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
 
-if(BMIMark > BMIJohn){
-    console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}`);
-}else{
-    console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}`);
-}
+// if(BMIMark > BMIJohn){
+//     console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}`);
+// }else{
+//     console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}`);
+// }
 
+
+// type conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+console.log(String(23), 23);
+
+// type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' + '10' + 3);
+console.log('23' * '10');
+console.log('23' / '10');
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n);
 
 

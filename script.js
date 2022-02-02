1560f1e85b830c9c234baa349d361692c9de7d8a
Akho0122 <mr.akhror.dev@gmail.com>
2022-02-02 21:34:13 +0500

    if / else statements

--- script.js
+++ script.js
@@ -39,22 +39,41 @@
 // console.log(BMIMark, BMIJohn, markHigherBMI);
 
 
-const firstName = "Jonas";
-const job = 'teacher';
-const birthYear = 1991;
-const year = 2037;
+// const firstName = "Jonas";
+// const job = 'teacher';
+// const birthYear = 1991;
+// const year = 2037;
 
-const jonas = "I'm " + firstName +
- ", a " +  (year - birthYear)
-  + " years old " + job + "!";  
-console.log(jonas);
+// const jonas = "I'm " + firstName +
+//  ", a " +  (year - birthYear)
+//   + " years old " + job + "!";  
+// console.log(jonas);
 
-const jonasNew = `I'm ${firstName},a ${year - 
-    birthYear} year old ${job}!`;
+// const jonasNew = `I'm ${firstName},a ${year - 
+//     birthYear} year old ${job}!`;
 
-console.log(jonasNew);
-console.log(`Just a regular string...`);
-console.log('String with \n\ multiple \n\ lines');
+// console.log(jonasNew);
+// console.log(`Just a regular string...`);
+// console.log('String with \n\ multiple \n\ lines');
+
+const age = 17;
+
+if(age >= 18){
+    console.log('Sarah can start driving license')
+}else{
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. 
+    Wait another ${yearsLeft} years:)`)
+}
+const birthYear = 1998;
+let century = 21;
+if(birthYear <= 2000){
+   century = 20;
+}else{
+   century = 21;
+}
+console.log(century);
+
 
 
 

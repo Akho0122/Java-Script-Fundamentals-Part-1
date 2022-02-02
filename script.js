683c4c3651116eb33e9826c91326d7ebfe148113
Akho0122 <mr.akhror.dev@gmail.com>
2022-02-02 21:21:50 +0500

    Template literals

--- script.js
+++ script.js
@@ -27,16 +27,34 @@
 
 // console.log(now - 1991 > now - 2018);
 
-const massMark = 78;
-const heightMark= 1.69;
-const massJohn = 92;
-const heightJohn = 1.95
+// const massMark = 78;
+// const heightMark= 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95
 
-const BMIMark = massMark / heightMark ** 2;
-const BMIJohn = massJohn / (heightJohn * heightJohn);
-const markHigherBMI = BMIMark > BMIJohn;
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// const markHigherBMI = BMIMark > BMIJohn;
 
-console.log(BMIMark, BMIJohn, markHigherBMI);
+// console.log(BMIMark, BMIJohn, markHigherBMI);
+
+
+const firstName = "Jonas";
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName +
+ ", a " +  (year - birthYear)
+  + " years old " + job + "!";  
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName},a ${year - 
+    birthYear} year old ${job}!`;
+
+console.log(jonasNew);
+console.log(`Just a regular string...`);
+console.log('String with \n\ multiple \n\ lines');
 
 
 

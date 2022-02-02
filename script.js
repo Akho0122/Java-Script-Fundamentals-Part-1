c3905e3b3ac09a28ae4566e7c43680bc0c9a236c
Akho0122 <mr.akhror.dev@gmail.com>
2022-02-02 21:02:03 +0500

    Assignment 1

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2010;
-console.log(ageJonas, ageSarah);
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2010;
+// console.log(ageJonas, ageSarah);
 
-console.log(ageJonas * 2, ageJonas / 2, 2 ** 3);
-// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+// console.log(ageJonas * 2, ageJonas / 2, 2 ** 3);
+// // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
 
-const firstName = 'Jonas';
-const lastName = 'Schmedtmann';
-console.log(firstName + " " + lastName);
+// const firstName = 'Jonas';
+// const lastName = 'Schmedtmann';
+// console.log(firstName + " " + lastName);
 
 
-// Assignment operators
-let x = 10 + 5; // 15
-x += 10; // x = x + 10 = 25
-x *= 4; // x = x * 4 = 100
-x++;
-x--;
-console.log(x);
+// // Assignment operators
+// let x = 10 + 5; // 15
+// x += 10; // x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x++;
+// x--;
+// console.log(x);
 
-// Comparison operators
-console.log(ageJonas > ageSarah); // >, <, >=, <=
-console.log(ageSarah >= 18);
+// // Comparison operators
+// console.log(ageJonas > ageSarah); // >, <, >=, <=
+// console.log(ageSarah >= 18);
 
-const isFullAge = ageSarah >= 18;
+// const isFullAge = ageSarah >= 18;
 
-console.log(now - 1991 > now - 2018);
+// console.log(now - 1991 > now - 2018);
+
+const massMark = 78;
+const heightMark= 1.69;
+const massJohn = 92;
+const heightJohn = 1.95
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
+
+
+
